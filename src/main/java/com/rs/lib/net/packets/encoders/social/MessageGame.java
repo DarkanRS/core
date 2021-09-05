f379ae83ac26a930c70fd1505bd92e4bc1861992
Trent <trenton.kress@gmail.com>
2021-09-05 08:02:31 -0700

    Refactor message types

--- src/main/java/com/rs/lib/net/packets/encoders/social/MessageGame.java
+++ src/main/java/com/rs/lib/net/packets/encoders/social/MessageGame.java
@@ -10,6 +10,14 @@ public class MessageGame extends PacketEncoder {
 	private MessageType type;
 	private String message;
 	private Account target;
+	
+	public MessageGame(String message) {
+		this(MessageType.UNFILTERABLE, message, null);
+	}
+	
+	public MessageGame(MessageType type, String message) {
+		this(type, message, null);
+	}
 
 	public MessageGame(MessageType type, String message, Account target) {
 		super(ServerPacket.GAME_MESSAGE);
@@ -42,10 +50,39 @@ public class MessageGame extends PacketEncoder {
 
 	public enum MessageType {
 		UNFILTERABLE(0),
-		FILTERABLE(109),
+		STAFF_CHAT(1),
+		PUBLIC_CHAT(2),
+		PRIVATE_MESSAGE(3),
+		ENGINE(4),
+		FC_NOTIFICATION(11),
+		ITEM_EXAMINE(27),
+		NPC_EXAMINE(28),
+		OBJECT_EXAMINE(29),
+		FRIEND_NOTIFICATION(30),
+		IGNORE_NOTIFICATION(31),
 		DEV_CONSOLE_CLEAR(98),
 		DEV_CONSOLE(99),
-		TRADE_REQUEST(100);
+		TRADE_REQUEST(100),
+		DUEL_REQUEST(101),
+		ASSIST_REQUEST(102),
+		UNK_103(103),
+		UNK_104(104),
+		UNK_CHALLENGE_105(105),
+		UNK_CHALLENGE_106(106),
+		UNK_CHALLENGE_107(107),
+		ALLIANCE_REQUEST(108),
+		FILTERABLE(109),
+		UNK_GAME_110(110),
+		DUNGEONEERING_INVITE(111),
+		VOTE_REQUEST(112),
+		UNK_CHALLENGE_113(113),
+		UNK_CHALLENGE_114(114),
+		UNK_DARK_RED_115(115),
+		UNK_PLAIN_NOTIFICATION_116(116),
+		CLAN_INVITE(117),
+		CLAN_CHALLENGE_REQUEST(118),
+		UNK_119(119),
+		UNK_120(120);
 		
 		private int value;
 		
