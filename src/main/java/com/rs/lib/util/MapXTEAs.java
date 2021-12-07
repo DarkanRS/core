e7d06302a72cef0e25d415759da7a7231d1881f9
Trent <trenton.kress@gmail.com>
2021-12-07 10:22:16 -0800

    Update xteas to json format

--- src/main/java/com/rs/lib/util/MapXTEAs.java
+++ src/main/java/com/rs/lib/util/MapXTEAs.java
@@ -16,94 +16,32 @@
 //
 package com.rs.lib.util;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.channels.FileChannel.MapMode;
 import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.JsonIOException;
+import com.google.gson.reflect.TypeToken;
+import com.rs.lib.file.JsonFileManager;
 
 public final class MapXTEAs {
-
-	private final static HashMap<Integer, int[]> XTEA_KEYS = new HashMap<Integer, int[]>();
-	private final static String PACKED_PATH = "data/map/archiveKeys/packed.mcx";
+	
+	private static Map<Integer, int[]> KEYS = new HashMap<>();
+	private final static String PATH = "./data/map/xteaKeys.json";
 
 	public static final int[] getMapKeys(int regionId) {
-		if (XTEA_KEYS.isEmpty())
-			loadKeys();
-		int[] arr = XTEA_KEYS.get(regionId);
+		int[] arr = (int[]) KEYS.get(regionId);
 		if (arr == null || (arr[0] == 0 && arr[1] == 0 && arr[2] == 0 && arr[3] == 0))
 			return null;
 		return arr;
 	}
 
-	public static void loadKeys() {
-		if (new File(PACKED_PATH).exists())
-			loadPackedKeys();
+	public static void loadKeys() throws JsonIOException, IOException {
+		if (!new File(PATH).exists())
+			throw new FileNotFoundException("No map keys file found!");
 		else
-			loadUnpackedKeys();
+			KEYS = JsonFileManager.loadJsonFile(new File(PATH), new TypeToken<Map<Integer, int[]>>(){}.getType());
 	}
-
-	private static final void loadPackedKeys() {
-		try {
-			RandomAccessFile in = new RandomAccessFile(PACKED_PATH, "r");
-			FileChannel channel = in.getChannel();
-			ByteBuffer buffer = channel.map(MapMode.READ_ONLY, 0, channel.size());
-			while (buffer.hasRemaining()) {
-				int regionId = buffer.getShort() & 0xffff;
-				int[] xteas = new int[4];
-				for (int index = 0; index < 4; index++)
-					xteas[index] = buffer.getInt();
-				XTEA_KEYS.put(regionId, xteas);
-			}
-			channel.close();
-			in.close();
-		} catch (Throwable e) {
-			Logger.handle(e);
-		}
-	}
-
-	public static final void loadUnpackedKeys() {
-		Logger.log("MapArchiveKeys", "Packing map containers xteas...");
-		try {
-			DataOutputStream out = new DataOutputStream(new FileOutputStream(PACKED_PATH));
-			File unpacked = new File("data/map/archiveKeys/unpacked/");
-			File[] xteasFiles = unpacked.listFiles();
-			for (File region : xteasFiles) {
-				String name = region.getName();
-				if (!name.contains(".txt")) {
-					region.delete();
-					continue;
-				}
-				int regionId = Short.parseShort(name.replace(".txt", ""));
-				if (regionId <= 0) {
-					region.delete();
-					continue;
-				}
-				BufferedReader in = new BufferedReader(new FileReader(region));
-				out.writeShort(regionId);
-				final int[] xteas = new int[4];
-				for (int index = 0; index < 4; index++) {
-					xteas[index] = Integer.parseInt(in.readLine());
-					out.writeInt(xteas[index]);
-				}
-				XTEA_KEYS.put(regionId, xteas);
-				in.close();
-			}
-			out.flush();
-			out.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private MapXTEAs() {
-
-	}
-
 }
